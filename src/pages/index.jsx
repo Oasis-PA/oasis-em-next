0d137080245197c50d5adae7a4a9420cb8bad6a4
stefano3938 <sasukeuchihaps338@gmail.com>
2025-08-21 23:09:47 -0300

    adicionando arquivos do projeto

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Blog Oasis - Página Inicial</h1>
+      <p>Se você está vendo esta página, o Next.js está funcionando!</p>
+    </div>
+  );
+}
