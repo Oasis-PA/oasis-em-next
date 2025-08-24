90ad3b11ee93f72aa3c691cbe6db2e05ffbc60ec
stefano3938 <sasukeuchihaps338@gmail.com>
2025-08-24 20:18:56 -0300

    fazendo uita coisa

---
+++ src/app/teste/page.tsx
@@ -0,0 +1,15 @@
+
+import {Cabecalho} from '../../components';
+
+export default function TestePage() {
+  return (
+  <html>
+     <body>
+     <Cabecalho />
+     </body>
+  </html>
+    
+     
+   
+  );
+}
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+// components/index.ts
+export { default as Cabecalho } from './cabecalho';
+//export { default as Rodape } from './rodape';
+//export { default as Botao } from './Botao';
