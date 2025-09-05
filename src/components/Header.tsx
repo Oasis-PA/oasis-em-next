e9fdac6e30e42bdb411703aa37638fe3ccaa63e7
JuliaBragaF <julia.b.freitas@ba.estudante.senai.br>
2025-09-05 16:50:36 -0300

    Revert "."
    This reverts commit 40cb5e5044e4efe422056fbfa7bfc0970ab4b136.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import "@/styles/Header.module.css"; 
+import "@/styles/cabecalho.css"; 
 import "@/styles/globals.css";
 
 export default function Header() {
