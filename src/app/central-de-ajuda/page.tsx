f0604f557e93a7a1a0d2d9366ded3f0750dc5a44
arturtistis <artuh.conceicao@ba.estudante.senai.br>
2025-09-13 19:09:43 -0300

    criei pasta central de ajuda

---
+++ src/app/central-de-ajuda/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import {Header, Footer} from "@/components";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import "@/styles/central-de-ajuda.css";
+
+export default function Centraldeajuda() {
+  return (
+    <main>
+      <h1>Olá, Mundo!</h1>
+      <p>Bem-vindo à minha primeira página Next.js com TSX.</p>
+    </main>
+  );
+}
