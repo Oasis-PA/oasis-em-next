89a2896e6e57314fb290784959e67ab9d6174acc
arturtistis <artuh.conceicao@ba.estudante.senai.br>
2025-09-14 19:34:24 -0300

    stefano resolve essa porra desse bagulho plmds

---
+++ src/app/infantil/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import {Header, Footer} from "@/components";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import "@/styles/infantil.css";
+
+export default function infantil() {
+  return (
+    <>
+    <main>
+        <div id="d1">
+            <h1></h1>
+            <p></p>
+        </div>
+    </main>
+    </>
+  );
+}
