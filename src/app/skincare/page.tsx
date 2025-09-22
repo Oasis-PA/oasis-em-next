df2fb4e48435349b20864640af95696404c1fa49
arturtistis <artuh.conceicao@ba.estudante.senai.br>
2025-09-22 18:07:28 -0300

    ok

--- src/app/skincare/page.tsx
+++ src/app/skincare/page.tsx
@@ -10,10 +10,16 @@ import "@/styles/skincare.css";
 export default function skincare() {
   return (
     <>
-    
+    <header>
+      <h1>SKIN</h1>
+      <h2>CARE</h2>
+    </header>
     
     <main>
-
+      <section id="s1">
+        <h1>Skincare com Propósito: Cuidar de Todos os Tons de Beleza</h1>
+        <div id="tons"></div>
+      </section>
     </main>
     </>
   );
