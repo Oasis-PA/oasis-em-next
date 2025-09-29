ca483588dc264bb928a148a3d189c099c45b431d
arturtistis <artuh.conceicao@ba.estudante.senai.br>
2025-09-29 11:48:02 -0300

    espero que julia *********************

--- src/app/meuperfil-before/page.tsx
+++ src/app/meuperfil-before/page.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Link from 'next/link'; // Importando o componente Link do Next.js para navegação
+import Link from 'next/link'; 
 
+import '@/styles/meuperfil-before.css';
 
-
-import '@/styles/avatar.css';
-// A importação de fontes geralmente é feita em um layout global, mas mantive aqui para seguir a estrutura original.
 
 
 
@@ -22,13 +20,11 @@ const MeuAvatarPage: React.FC = () => {
         <div className="sec2-texto">
           <h1 className="titulo">CRIE SEU PERFIL</h1>
           <p className="texto">Sua identidade, suas regras. Comece a montar agora!</p>
-          {/* Usando o componente Link do Next.js para a navegação */}
           <Link href="/crie-agora">CRIE AGORA</Link>
         </div>
 
         <div className="sec2-imagem">
-          {/* O caminho da imagem agora é absoluto a partir da pasta 'public' */}
-          <img src="/images/Avatar/scorza.png" alt="Avatar" />
+          <img src="/images/meuperfil-before/scorza.png" alt="Avatar" />
         </div>
       </section>
     </>
