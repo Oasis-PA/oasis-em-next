1a249a60890c240a0cbde4eebdbc75ca2b7d73ef
arturtistis <artuh.conceicao@ba.estudante.senai.br>
2025-10-14 09:51:51 -0300

    mds ficou pft q benção

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,85 @@
-import { Header, Footer } from "@/components";
+"use client";
 
+import { useState, useEffect } from 'react';
 import Image from "next/image";
 import Link from "next/link";
-
 import styles from '@/styles/page.module.css';
 
+const slidesData = [
+  {
+    h2: "Tratamentos inovadores",
+    h1: "CUIDADO SEM LIMITES",
+    backgroundImage: "/images/tela-principal/banner1.png",
+    link: "/cronograma-capilar"
+  },
+  {
+    h2: "Descubra seu melhor visual",
+    h1: "REINVENTE SEU ESTILO",
+    backgroundImage: "/images/tela-principal/banner2.png",
+    link: "/haircare"
+  },
+  {
+    h2: "O melhor de uma pele renovada",
+    h1: "BELEZA QUE EMPODERA",
+    backgroundImage: "/images/tela-principal/banner3.png",
+    link: "/skincare"
+  }
+];
+
 export default function OasisHomepage() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentSlide(prevSlide => (prevSlide + 1) % slidesData.length);
+    }, 5000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const handleDotClick = (index: number) => {
+    setCurrentSlide(index);
+  };
+
   return (
-      <>
-      <main className={styles.mainContainer}>
-        <h2>Tratamentos inovadores</h2>
-        <h1>Cuidado sem limites</h1>
-        <button>CONHEÇA</button>
-        <div>
-          <Link href="#"><img src="/images/tela-principal/bolinha-marcada.svg" alt="" /></Link>
-          <Link href="#"><img src="/images/tela-principal/bolinha-naomarcada.svg" alt="" /></Link>
-          <Link href="#"><img src="/images/tela-principal/bolinha-naomarcada.svg" alt="" /></Link>
+    <>
+      <main 
+        className={styles.mainContainer} 
+        style={{ backgroundImage: `url(${slidesData[currentSlide].backgroundImage})` }}
+      >
+        <h2>{slidesData[currentSlide].h2}</h2>
+        <h1>{slidesData[currentSlide].h1}</h1>
+
+        <Link 
+          href={slidesData[currentSlide].link} 
+          className={styles.conhecaButton}
+        >
+          CONHEÇA
+        </Link>
+
+        <div className={styles.dotsContainer}>
+          {slidesData.map((_, index) => (
+            <button 
+              key={index} 
+              onClick={() => handleDotClick(index)}
+              className={styles.dotButton}
+            >
+              <Image 
+                src={
+                  currentSlide === index 
+                    ? "/images/tela-principal/bolinha-marcada.svg" 
+                    : "/images/tela-principal/bolinha-naomarcada.svg"
+                } 
+                alt={`Ir para o slide ${index + 1}`}
+                width={20}
+                height={20}
+              />
+            </button>
+          ))}
         </div>
       </main>
-      </>
+
+      
+    </>
   );
 }
