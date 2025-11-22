02ffe20db2e4f45b88dd9cba6ba1b008d7f7c3e1
henriquesantoss7 <henrique.c.santos7@ba.estudante.senai.br>
2025-11-22 12:03:05 -0300

    mudando novidades pag princ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -428,25 +428,25 @@ export default function OasisHomepage() {
           <div className={styles.cardsnovidades}>
             <Link href='/produtos'>
               <div className={styles.cardnovidade1}>
-                <h1>Creme de Pentear Phytomanga Efeito Pesado 500ml</h1>
+                <h1>Umidificador para cachos - Vizeme</h1>
               </div>
             </Link>
 
             <Link href='/produtos'>
               <div className={styles.cardnovidade2}>
-                <h1>EFFACLAR REEQUILIBRANTE</h1>
+                <h1>Esfoliante Nutritivo Tododia Jambo Rosa e Flor de Caju</h1>
               </div>
             </Link>
 
             <Link href='/produtos'>
               <div className={styles.cardnovidade3}>
-                <h1>produto</h1>
+                <h1>Sérum Hidratante Principia 2% Ácidos Hialuronicos + B5</h1>
               </div>
             </Link>
 
             <Link href='/produtos'>
               <div className={styles.cardnovidade4}>
-                <h1>produto</h1>
+                <h1>Hidratante Corporal Pele Negra Vegano Raavi</h1>
               </div>
             </Link>
           </div>
